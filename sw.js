02de66fcac6b986323b928918b0fd1a26172a905
tongzildjian <tong_zildjians@hotmail.com>
2026-02-24 10:17:28 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'finance-tracker-cache-v1';
+const urlsToCache = [
+    './tongzhome.html',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
